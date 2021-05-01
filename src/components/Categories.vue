e4a87e977c3d350a69c0c102eadf949396e9e89b
Dai Alaa <dai.a.elrihany@gmail.com>
2021-05-01 23:49:40 +0200

    home background

---
+++ src/components/Categories.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <!-- <v-card class="category">
+        <v-container>
+          <v-row>
+            <v-col class=" d-flex flex-column title">
+                <v-card-title>Cameras</v-card-title>
+                <v-card-subtitle></v-card-subtitle>
+                <v-img src="../assets/logo.png"></v-img>
+                <v-card-actions>
+                <v-btn>Listen Now</v-btn>
+                </v-card-actions>
+            </v-col>
+            <v-col>
+              <v-img src="../assets/logo.png"></v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card> -->
+      <v-card> 
+        <v-img id="image" src="../assets/logo.png" height="600"> 
+          <v-card-title class="headline"> Search for <v-spacer></v-spacer>
+            <span class="caption font-italic font-weight-light">yes</span>
+          </v-card-title> 
+        </v-img> 
+      </v-card>
+  </div>
+</template>
+<style lang="scss" scoped>
+.category {
+  width:500px;
+  height: 100%;
+  background-color: rgb(163, 211, 233);
+  margin-left: 41%;
+}
+.title {
+    width:50%;
+    height: 30%;
+}
+</style>
+<script>
+export default {
+    
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,98 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="home">
+      <button class="sign">
+        SIGN UP
+      </button>
+      <button class="log">
+        LOG IN
+      </button>
+      <p class="search">
+        What's in your mind?
+      </p>
+      <input class="searchInput" placeholder="Tap to search .."/>
+      <Categories />
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.home {
+  background-image: url("../assets/backGround.jpg");
+  background-repeat: no-repeat;
+  // background-size:100%;
+  // background-position-x: 100%;
+  height: calc(100vh);
+}
+.sign {
+  border: none;
+  border-radius: 20px;
+  width: 6%;
+  height: 6%;
+  margin-left: 84%;
+  margin-top: 2%;
+  position: fixed;
+  background-color: white;
+  color: rgb(15, 21, 73);
+  font-weight: bold;
+  text-decoration: none;
+  outline: none;
+}
+.log {
+  border: none;
+  border-radius: 20px;
+  width: 6%;
+  height: 6%;
+  margin-top: 2%;
+  margin-left: 91%;
+  position: fixed;
+  background-color: rgb(15, 21, 73);
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+  outline: none;
+}
+.sign:hover, .log:hover {
+  height: 6.5%;
+}
+.searchInput {
+  margin-top: 10%;
+  width: 22%;
+  height: 5%;
+  border-radius: 20px;
+  margin-left: 40%;
+  text-decoration: none;
+  outline: none;
+  border: none;
+  padding-left: 1%;
+  margin-bottom: 5%;
+}
+.search {
+  margin-left: 30%;
+  padding-top: 8%;
+  width: 10%;
+  height: 5%;
+  font-size: 20px;
+  color: rgb(46, 53, 114);
+  position:absolute;
+  font-family: cursive;
+}
+</style>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Categories from "@/components/Categories.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Categories
   },
+  computed: {
+  heroImage() {
+    return {
+      backgroundImage: `url(${require('../assets/backGround.jpg')})`
+    };
+  }
+}
 };
 </script>
